51bff75e54844bba9830367f559927136ec6a7de
MuminjonM <m.o.murodullayev@gmail.com>
2024-10-26 09:52:38 +0500

    3- versiya  4 ta son uchun ekub

--- darsKIDT/Program.cs
+++ darsKIDT/Program.cs
@@ -10,17 +10,26 @@ namespace darsKIDT
     {
         static void Main(string[] args)
         {
-            int a = 24, b = 36;
-            
-            while(a!=b)
+            int a = 24, b = 36, c=72, d=100;
+
+            int ab = ekub(a, b);
+            int cd= ekub(c, d);
+            int natija=ekub(ab, cd);
+
+            Console.WriteLine("ekub= " + natija);
+            Console.ReadKey();
+        }
+
+        public static int ekub(int a,int b) {
+
+
+            while (a != b)
             {
                 if (a > b) a -= b;
                 else
                     b -= a;
             }
-
-            Console.WriteLine("ekub= " + a);
-            Console.ReadKey();
+            return a;
         }
     }
 }
